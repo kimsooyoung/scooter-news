e90266760ca1c56923a1c21c5ac68b04acd6f093
kimsooyoung <tge1375@naver.com>
2019-11-25 15:12:25 +0900

    Updates

---
+++ precache-manifest.efd02c17c796f0dc015bfc3aa8847369.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04cedec837c66f620a35cb72879ce869",
+    "url": "/scooter-news/index.html"
+  },
+  {
+    "revision": "457318ce08eeb33fec5e",
+    "url": "/scooter-news/static/css/main.11ce144e.chunk.css"
+  },
+  {
+    "revision": "7dc18f40d05de3cd216e",
+    "url": "/scooter-news/static/js/2.9ec3a0db.chunk.js"
+  },
+  {
+    "revision": "457318ce08eeb33fec5e",
+    "url": "/scooter-news/static/js/main.815746e1.chunk.js"
+  },
+  {
+    "revision": "4d44389623f8936fd5ab",
+    "url": "/scooter-news/static/js/runtime-main.37ce3965.js"
+  }
+]);
